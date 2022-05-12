4a19d68c540af77808c7dc5f2d92240042cd8838
brooklyn-hoe <brooklyn_hoe@student.asdah.school.nz>
2022-05-12 15:13:49 +1200

    did my first question and started on my second

---
+++ quiz.py
@@ -0,0 +1,20 @@
+print("welcome to the anime quiz. this quiz is about animes. so lets test your anime knowledge.")
+
+score = 0
+q1 = input("Question 1)What is the most popular anime? \n One Piece \n Naruto \n Dragon Ball \n Fullmetal Alchemist.")
+
+if q1 == "Dragon ball":
+    print("thats correct you get one point")
+    score += 1
+elif q1 == "One piece" or q1 == "Naruto" or q1 == "Fullmetal Alchemist":
+    print("incorrect")
+    score -= 1
+else:
+    print("sorry that is not an option")
+    score -= 1
+
+q2 = input(" Question 2)Which anime character is stronger? \n Luffy \n Naruto \n Asta \n Itadori.")
+
+if q2 == "Luffy":
+    score += 1
+elif q2 == 
