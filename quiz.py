b4e6886b72733e864674dcd9a27400fe9bc312bb
brooklyn-hoe <brooklyn_hoe@student.asdah.school.nz>
2022-05-17 12:27:07 +1200

    finshed question 4

--- quiz.py
+++ quiz.py
@@ -3,12 +3,12 @@ print("welcome to the anime quiz. this quiz is about animes. so lets test your a
 
 #Question 1
 score = 0
-q1 = input("Question 1)What is the most popular anime? \n One Piece \n Naruto \n Dragon Ball \n Fullmetal Alchemist.")
+q1 = input("Question 1)What is the most popular anime? \n One Piece \n Naruto \n Dragon Ball \n Fullmetal Alchemist").lower()
 
-if q1 == "Dragon ball":
+if q1 == "dragon ball":
     print("thats correct you get one point")
     score += 1
-elif q1 == "One piece" or q1 == "Naruto" or q1 == "Fullmetal Alchemist":
+elif q1 == "one piece" or q1 == "naruto" or q1 == "fullmetal alchemist":
     print("incorrect")
     score -= 1
 else:
@@ -16,12 +16,12 @@ else:
     score -= 1
 
 #Question 2
-q2 = input(" Question 2)Which anime character is stronger? \n Luffy \n Naruto \n Asta \n Itadori.")
+q2 = input(" Question 2)Which anime character is stronger? \n Luffy \n Naruto \n Asta \n Itadori").lower()
 
-if q2 == "Luffy":
+if q2 == "luffy":
     print("thats correct you get one point")
     score += 1
-elif q2 == "Naruto" or q2 == "Asta" or q2 == "Itadori":
+elif q2 == "naruto" or q2 == "asta" or q2 == "itadori":
     print("Incorrect you lose a point")
     score -= 1
 else:
@@ -29,17 +29,28 @@ else:
     score -= 1
 
 #Question 3
-q3 = input(" Question 3) Which anime has the most episode? \n Dragon Ball \n Death Note \n Black Clover \n Fullmetal Alchemist.")
+q3 = input(" Question 3) Which anime has the most episode? \n Dragon Ball \n Death Note \n Black Clover \n Fullmetal Alchemist").lower()
 
-if q3 == "Black Clover":
+if q3 == "black clover":
     print("Thats correct you get one point")
     score += 1
-elif q3 == "Dragon Ball" or q3 == "Death Note" or q3 == "Fullmetal Alchemist":
+elif q3 == "dragon ball" or q3 == "death note" or q3 == "fullmetal alchemist":
     print("Incorrect you lose one point")
     score -= 1
 else:
     print("Sorry thats not an option")
     score -= 1
 
+#Question 4
+q4 = input(" Question 4) Who died first out of thease characters from naruto? \n Itachi \n Jiraiya \n Rin \n Kurama").lower()
 
+if q4 == "rin":
+    print("Thats correct you get one point")
+    score += 1
+elif q4 == "itachi" or q4 == "jiraiya" or q4 == "kurama":
+    print("Incorrect you lose one point")
+    score -= 1
+else:
+    print("Sorry thats not an option")
+    score -= 1
 
